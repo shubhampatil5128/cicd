72c5709a0e093ad6a6435381adb4aaf55585ba38
shubhampatil5128 <31090767+shubhampatil5128@users.noreply.github.com>
2022-04-09 20:40:59 +0530

    Create SampleWebApplicationTests.java

---
+++ src/test/java/com/example/demo/SampleWebApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+© 2022 GitHub, Inc.
